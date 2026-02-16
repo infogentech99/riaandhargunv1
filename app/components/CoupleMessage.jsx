ebbd8b5291d23acbbecf26d3211479405061235a
unknown <md.fardeen5236@gmail.com>
2026-02-16 14:33:06 +0530

    show slider image

--- app/components/CoupleMessage.jsx
+++ app/components/CoupleMessage.jsx
@@ -34,12 +34,12 @@ export default function CoupleMessage() {
     return (
 
         <div className="bg-[url('/assets/backmid.png')] bg-cover bg-no-repeat">
-            <div className="lg:h-450 md:h-520 h-365">
+             <div className="lg:h-592 md:h-520 h-520">
                 <h1 className="lg:text-[50px] md:text-3xl text-[52px] text-center text-[#DEE6FF] lg:pt-32 pt-12 font-cormorant-upright">A message from the couple</h1>
                 <h2 className="lg:text-[30px] text-[22px]  text-center text-[#DEE6FF] lg:px-60 px-6 lg:mt-28 mt-12 font-cormorant-upright lg:leading-8 md:leading-8 leading-6">
                     From different traditions to one beautiful journey, join us as we celebrate love, laughter, and forever.
                     This moment wouldn’t be the same without the people we love most. Thank you for your love, blessings, and for making our journey even more special, we’re so excited to celebrate together!</h2>
-                {/* <div className="mt-32 flex justify-center items-center">
+                 <div className="mt-32 flex justify-center items-center">
                     <Swiper
                         modules={[Autoplay, Pagination]}
                         autoplay={{ delay: 3500, disableOnInteraction: false }}
@@ -70,7 +70,8 @@ export default function CoupleMessage() {
                             </SwiperSlide>
                         ))}
                     </Swiper>
-                </div> */}
+                </div> 
+                
                 <div className="flex flex-col-1 justify-center items-center mt-16">
                     <div className="bg-[url('/assets/seap_h.png')] bg-cover bg-no-repeat lg:h-150 lg:w-150 h-95 w-95 md:h-120 md:w-120">
                         <h1 className="lg:text-6xl md:text-5xl text-[40px] leading-10 font-bold lg:leading-12 lg:font-semibold text-center text-[#3c2267] lg:pt-26 md:pt-22 pt-16 font-cormorant-upright">
